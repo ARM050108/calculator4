ca62812f04c7d587de3bbaa46cdf85469c0b8d28
Алекс <aleks@MacBook-Air-Aleks.local>
2024-06-23 11:37:01 +0300

    Initial commit

---
+++ src/main/java/Calcul.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+public class Calcul {
+    public static void main(String[] args) throws Exception {
+
+        Scanner scanner = new Scanner(System.in); //создали сканнер
+        System.out.println("Введите только два числа (арабских или римских): ");
+        String input = scanner.nextLine(); //считали с консоли выражение пользователя
+        System.out.println(calc(input)); // и передаем его в метод парси, где оно и будет вычисляться
+    }
+        public static String calc (String input) throws Exception {
+
+        int num1 = 0;
+        int num2 = 0;
+        Ro roman1;
+        Ro roman2;
+        String sign;
+        String result;
+        boolean getObject = true;
+        String[] terms = input.split("[+\\-*/]");
+        if (terms.length != 2) throw new Exception("т.к. должно быть только ровно два слагаемых");
+
+        sign = detectOperation(input);
+
+        if (Ro.getObject(terms[0]) && Ro.getObject(terms[1])) {
+            num1 = Ro.valueOf(terms[0]).getArabian();
+            num2= Ro.valueOf(terms[1]).getArabian();
+            getObject = true;
+        }
+        else if (!Ro.getObject(terms[0]) && !Ro.getObject(terms[1]))
+            {num1 = Integer.parseInt(terms[0]);
+            num2 = Integer.parseInt(terms[1]);
+            getObject = false;}
+        else {
+            System.out.println("throw Exception // т.к используются одновременно разные системы счисления");
+        }
+        if (num1 > 10 || num2 > 10) {
+                        System.out.println("throw Exception");
+                    }
+                    int arabian =  calc(num1, num2, sign);
+            if (getObject) {
+
+                if (arabian <= 0) {
+                    System.out.println("throw Exception // т.к. в римской системе нет отрицательныхчисел");
+                }
+                //конвертируем результат операции из арабского в римское
+
+                result = Ro.getRoman(arabian).toString();
+
+            } else {
+                //Конвертируем арабское число в тип String
+                result = String.valueOf(arabian);
+            }
+//                    //возвращаем результат
+                    return result;
+                }
+            static String detectOperation(String input) {
+            if (input.contains("+")) return "+";
+            else if (input.contains("-")) return "-";
+            else if (input.contains("*")) return "*";
+            else return "/";
+    }
+            static int calc(int a, int b, String sign) {
+
+            if (sign.equals("+")) return a + b;
+            else if (sign.equals("-")) return a - b;
+            else if (sign.equals("*")) return a * b;
+            else return a / b;
+    }
+}
+
+
+
+
+
+
+
---
+++ src/main/java/Ro.java
@@ -0,0 +1,48 @@
+public enum Ro {
+
+    I(1,"I"), II(2,"II"), III(3,"III"), IV(4,"IV"),
+    V(5,"V"), VI(6,"VI"), VII(7,"VII"), VIII(8,"VII"),
+    IX(9,"XI"), X(10,"X"), XI(11,"XI"), XII(12,"XII"), XIII(13,"XIII"),
+    XIV(14,"XIV"), XV(14,"XIV"), XVI(16,"XVI"), XVII(17,"XVII"), XVIII(18,"XVIII"),
+    XIX(19,"XIX"), XX(20,"XX"), XXI(21,"XXI"), XXIV(24,"XXIV"), XXV(25,"XXV"),
+    XXVII(27,"XXVII"), XXVIII(28,"XXVIII"), XXX(30,"XXX"), XXXII(32,"XXXII"), XXXVI(36,"XXXVI"),
+    XL(40,"XL"), XLII(42,"XLII"), XLV(45,"XLV"), XLVIII(48,"XLVIII"), XLIX(49,"XLIX"),
+    L(50,"L"), LIV(54,"LIV"), LVI(56,"LVI"), LX(60,"LX"), LXIII(63,"LXIII"),
+    LXIV(64,"LXIV"), LXX(70,"LXIV"), LXXII(72,"LXXII"), LXXX(80,"LXXX"), LXXXI(81,"LXXXI"),
+    XC(90,"XC"), C(100,"C");
+
+     public int arabian;
+     final String name;
+
+     Ro(int arabian, String name) {       // для этого мы создадим конструктор и в него передадим соотвествующую переменную
+        this.arabian = arabian; this.name = name;}
+     public int getArabian() {
+        return arabian;
+    }
+     static boolean getObject(String name ) {
+        for (Ro obj : Ro.values()) {
+            if(obj.name.equals(name)) return true;
+        }
+        return false;
+    }
+    static Ro getRoman(int arabian){
+        for (Ro rom: Ro.values()){
+            if(rom.arabian == arabian) return rom;
+        }
+        return null;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
